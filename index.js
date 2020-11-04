8f62ad82d41a2d9d56494d0b1030c7149cb8c226
Kamil-PLewka <kamilplewka12a@wp.pl>
2020-11-04 16:47:19 +0100

    repair code

---
+++ index.js
@@ -0,0 +1,3 @@
+document.getElementById("firsth6").innerHTML="Hello Dolly";
+document.getElementById("secoundh6").innerHTML="Hello Dolly";
+document.getElementById("thirdh6").innerHTML="Hello Dolly";
